83e428add3f6469b4509372b61f16159f1b51a05
Craig Riley <c.riley@evomark.co.uk>
2025-09-12 12:13:21 +0100

    Insert vuetify styles into layer and fix deps #patch

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,37 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import { libInjectCss } from "vite-plugin-lib-inject-css";
+
+function wrapVuetifyCssInLayer() {
+  return {
+    name: "wrap-vuetify-in-layers",
+    enforce: "post",
+    generateBundle(options, bundle) {
+      for (const [fileName, chunk] of Object.entries(bundle)) {
+        if (fileName.endsWith(".css") && chunk.type === "asset") {
+          chunk.source = `@layer vuetify { @layer components {\n${chunk.source}\n}}`;
+        }
+      }
+    },
+  };
+}
 
 export default defineConfig({
-	plugins: [vue(), libInjectCss()],
-	build: {
-		lib: {
-			entry: "resources/index.js",
-			formats: ["es", "cjs"]
-		},
-		rollupOptions: {
-			external: ["vue","vuetify", "@evomark/vue-forward-slots", "@inertiajs/vue3", "lodash-es", "axios", "@vueuse/core"]
-		}
-	}
+  plugins: [vue(), wrapVuetifyCssInLayer()],
+  build: {
+    lib: {
+      entry: "resources/index.js",
+      formats: ["es", "cjs"],
+    },
+    rollupOptions: {
+      external: [
+        "vue",
+        "vuetify",
+        "@evomark/vue-forward-slots",
+        "@inertiajs/vue3",
+        "lodash-es",
+        "axios",
+        "@vueuse/core",
+      ],
+    },
+  },
 });
